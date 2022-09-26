df5bac3efc0f40dc064774fac4c2847f54269ddc
Cindy <cindy.um73@gmail.com>
2022-09-26 16:35:09 +1000

    expense component built

--- expense-app/src/App.js
+++ expense-app/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const expenses = [
+    { 
+      title: "Grocceries", 
+      amount: 100, 
+      date: new Date(2022, 1, 28) 
+    },
+    { 
+      title: "Rent", 
+      amount: 500, 
+      date: new Date(2022, 1, 30) 
+    },
+    { 
+      title: "Travel", 
+      amount: 200, 
+      date: new Date(2022, 1, 15) 
+    },
+
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's Get Started:</h2>
+      <ExpenseItem 
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+
+      ></ExpenseItem>
     </div>
   );
+
 }
 
 export default App;
