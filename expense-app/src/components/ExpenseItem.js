c22b6598135fe5830020906c72441e8568ac3e70
Cindy <cindy.um73@gmail.com>
2022-09-27 21:15:49 +1000

    updated basic css

--- expense-app/src/components/ExpenseItem.js
+++ expense-app/src/components/ExpenseItem.js
@@ -1,18 +1,17 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from "./CardObj";
 import "./ExpenseItem.css";
-import ExpenseDate from "./ExpenseDate"
 
-function ExpenseItem(props){
 
+function ExpenseItem(props) {
     return(
-        <div className='expense-item'>
-            <ExpenseDate date={props.date}></ExpenseDate>
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item_description'>
                 <h2>{props.title} </h2>
-                <div className='expense-item__price'>${props.amount}</div>
-                
+                <div className='expense-item__price'>${props.amount}</div> 
             </div>
-        </div>
-
+        </Card>
     );
 }
 export default ExpenseItem;
