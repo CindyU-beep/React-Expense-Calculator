d05417df01ac2d97a085de5bdf25de5b97abe5dc
Cindy <cindy.um73@gmail.com>
2022-10-03 20:33:27 +1100

    added dynamic expense array

--- expense-app/src/App.js
+++ expense-app/src/App.js
@@ -1,34 +1,37 @@
+import React, { useState } from 'react';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/Expenses/NewExpense/NewExpense"
 
+const DUMMY_EXPENSE_DATA = [
+  { 
+    id: 'e1',
+    title: "Grocceries", 
+    amount: 100, 
+    date: new Date(2022, 1, 28) 
+  },
+  { 
+    id: 'e2',
+    title: "Rent",  
+    amount: 500, 
+    date: new Date(2022, 1, 30) 
+  },
+  { 
+    id: 'e3',
+    title: "Travel", 
+    amount: 200, 
+    date: new Date(2022, 1, 15) 
+  },
+
+];
+
 const App = () => { //alternative syntax using arrow functions
-
-  const expenses = [
-    { 
-      id: 'e1',
-      title: "Grocceries", 
-      amount: 100, 
-      date: new Date(2022, 1, 28) 
-    },
-    { 
-      id: 'e2',
-      title: "Rent",  
-      amount: 500, 
-      date: new Date(2022, 1, 30) 
-    },
-    { 
-      id: 'e3',
-      title: "Travel", 
-      amount: 200, 
-      date: new Date(2022, 1, 15) 
-    },
-
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSE_DATA);
 
   const addExpenseHandler = expense => {
-    console.log('In App js')
-    console.log(expense);
-  }
+    setExpenses(prevExpenses => {
+      return[expense,...prevExpenses]; //new expense as first data elem, copy all prev expenses
+    });
+  };
 
   return (
     <div>
